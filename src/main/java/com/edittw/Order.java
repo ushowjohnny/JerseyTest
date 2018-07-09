edc6b18ef2f486afde56abf3da204ac0b4221e8d
Newt <afbi1925@gmail.com>
2018-07-09 11:38:28 +0800

    create project

---
+++ src/main/java/com/edittw/Order.java
@@ -0,0 +1,62 @@
+package com.edittw;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+@Path("/order")
+public class Order {
+
+	// private static Logger logger = LoggerFactory.getLogger(Order.class);
+
+	private Object env;
+
+	public Object getEnv() {
+		return env;
+	}
+
+	public void setEnv(Object env) {
+		this.env = env;
+	}
+
+	@GET
+	@Path("louis")
+	public Object value() {
+		env = executeCommand("printenv");
+		System.out.println(env);
+		return Response.status(Status.OK).entity(env).build();
+	}
+
+	public String executeCommand(String command) {
+
+		StringBuffer output = new StringBuffer();
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
+
+	}
+
+	@GET
+	@Path("newt")
+	public String get() {
+		return "Hello, Newt";
+	}
+}
---
+++ src/test/java/com/edittw/OrderTest.java
@@ -0,0 +1,11 @@
+package com.edittw;
+
+import org.junit.Test;
+
+
+public class OrderTest {
+	@Test
+	public void test() {
+		System.out.println("Hello Test");
+	}
+}
