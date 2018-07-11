59c90a883cc3ea661b60eac703282a9a234a2f03
Newt <afbi1925@gmail.com>
2018-07-11 09:19:28 +0800

    modify pom

--- src/test/java/com/edittw/OrderTest.java
+++ src/test/java/com/edittw/OrderTest.java
@@ -1,9 +1,10 @@
 package com.edittw;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+@org.testng.annotations.Test
 public class OrderTest {
 
 	@Test
